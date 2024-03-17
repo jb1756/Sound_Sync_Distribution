a3f98ed39b4eba77b6e4b2fba3953e38547b5660
jb1756 <juliusbags04@yahoo.com>
2024-03-17 15:58:14 -0400

    added more bootstrap edits on about

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,30 +1,42 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap';
+import { Stack } from 'react-bootstrap';
 
 const About = () => {
   return (
-    <Container fluid>
-      <h1 stlye={{ textAlign: 'center', marginBottom: '20px' }}>ABOUT US</h1>
-      <Row style={{ marginBottom: '20px' }}>
-        Our mission is to amplify creativity and enrich experiences through the distribution of high-quality
+    <Stack gap={10}>
+      <div>
+        <h1 className="text-center text-primary mb-4">ABOUT US</h1>
+      </div>
+        <div className="h5 text-muted bg-light mb-4 p-4" style={{ marginBottom: '10px' }} >
+          <div>Our mission is to amplify creativity and enrich experiences through the distribution of high-quality
         audio proucts. We are dedicated to delivering innovative solutions that inspire and delight our customers,
         while fostering lasting partnerships with manufacturers and retailers. With a focus on excellence, 
         sustainability, and customer satisfaction, we strive to elevate the world of 
         audio technology and enhance the way people connect with sound.
-      </Row>
-      <h2 style={{ marginBottom: '10px' }}>Vision Statment</h2>
-      <Row style={{ marginBottom '20px' }}>We aim to become the premier distributor for discovering,
+          </div>
+      </div>
+
+      <div className="text-center text-primary mb-4" style={{ marginBottom: '10px' }}>
+        <h1>Vision Statment</h1>
+      </div>
+      <div className="h4 text-muted bg-light mb-4 p-4" style={{ marginBottom: '10px' }}>
+        <div>We aim to become the premier distributor for discovering,
         sharing and enjoying immersive audio experiences across all platforms
         and devices.
-      </Row>
-      <h2 style={{ marginBottom: '10px' }}>Goals</h2>
-      <Row style={{ marginBottom: '20px' }}>
-        <ul style={{ paddingLeft: '20px' }}>
-          <li>- On time delivery</li>
-          <li>- Customer satisfaction</li>
+        </div>
+      </div>
+
+      <div className="text-center text-primary mb-4" style={{ marginBottom: '10px' }}>
+        <h1>Goals</h1>
+      </div>
+
+      <div className= "h4 text-muted bg-light mb-4 p-4" style={{ marginBottom: '20px' }}>
+        <ul className="list-unstyled" style={{ marginBottom: '20px' }}>
+          <li>On time delivery</li>
+          <li>Customer satisfaction</li>
         </ul>
-      </Row>
-    </Container>
+      </div>
+    </Stack>
   )
 }
 
