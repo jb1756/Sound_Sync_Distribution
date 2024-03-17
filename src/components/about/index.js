36d3efcdf7d8d6ea01023869f96973ab985f37a7
jb1756 <juliusbags04@yahoo.com>
2024-03-17 13:00:57 -0400

    Added text on about me

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>about</div>
+    <div>
+      <h1>ABOUT US</h1>
+      <p>
+        Our mission is to amplify creativity and enrich experiences through the distribution of high-quality
+        audio proucts. We are dedicated to delivering innovative solutions that inspire and delight our customers,
+        while fostering lasting partnerships with manufacturers and retailers. With a focus on excellence, 
+        sustainability, and customer satisfaction, we strive to elevate the world of 
+        audio technology and enhance the way people connect with sound.
+      </p>
+      <h2>Vision Statment</h2>
+      <p>We aim to become the premier distributor for discovering,
+        sharing and enjoying immersive audio experiences across all platforms
+        and devices.
+      </p>
+      <h2>Goals</h2>
+      <p>
+        - On time delivery
+        - Customer satisfaction
+      </p>
+    </div>
   )
 }
 
