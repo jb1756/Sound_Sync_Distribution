ee1248232424eaa834985fd48c5a7d14f4cb996a
jb1756 <juliusbags04@yahoo.com>
2024-03-17 13:21:42 -0400

    Made editions on about/index.js using bootstrap

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,27 +1,28 @@
 import React from 'react'
+import { Container, Row } from 'react-bootstrap';
 
 const About = () => {
   return (
-    <div>
+    <Container>
       <h1>ABOUT US</h1>
-      <p>
+      <Row>
         Our mission is to amplify creativity and enrich experiences through the distribution of high-quality
         audio proucts. We are dedicated to delivering innovative solutions that inspire and delight our customers,
         while fostering lasting partnerships with manufacturers and retailers. With a focus on excellence, 
         sustainability, and customer satisfaction, we strive to elevate the world of 
         audio technology and enhance the way people connect with sound.
-      </p>
+      </Row>
       <h2>Vision Statment</h2>
-      <p>We aim to become the premier distributor for discovering,
+      <Row>We aim to become the premier distributor for discovering,
         sharing and enjoying immersive audio experiences across all platforms
         and devices.
-      </p>
+      </Row>
       <h2>Goals</h2>
-      <p>
+      <Row>
         - On time delivery
         - Customer satisfaction
-      </p>
-    </div>
+      </Row>
+    </Container>
   )
 }
 
