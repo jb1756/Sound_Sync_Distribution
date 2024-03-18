6b71c4013f50a5cae050bf8513ca3124ed6c0115
jb1756 <juliusbags04@yahoo.com>
2024-03-18 18:55:51 -0400

    added a navbar

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,8 +1,53 @@
 import React from 'react'
+import { Nav, Navbar} from 'react-bootstrap';
 
 const NavBar = () => {
   return (
-    <div>navBar</div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+    <Container fluid>
+      <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
+        <Nav
+          className="me-auto my-2 my-lg-0"
+          style={{ maxHeight: '100px' }}
+          navbarScroll
+        >
+          <Nav.Link href="#action1">Home</Nav.Link>
+          <Nav.Link href="#action2">Link</Nav.Link>
+          <NavDropdown title="Link" id="navbarScrollingDropdown">
+            <NavDropdown.Item href="#action3">
+              Products
+            </NavDropdown.Item>
+
+            <NavDropdown.Divider />
+
+            <NavDropdown.Item href="#action4">
+              Brands
+            </NavDropdown.Item>
+
+            <NavDropdown.Divider />
+
+            <NavDropdown.Item href="#action5">
+              Manufacturers
+            </NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="#" disabled>
+            Contact Us
+          </Nav.Link>
+        </Nav>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   )
 }
 
