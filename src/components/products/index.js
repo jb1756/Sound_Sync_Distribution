4f3749d9b21c33db42cac8e3b7465dfa03d340b9
jb1756 <juliusbags04@yahoo.com>
2024-03-20 20:49:30 -0400

    product page

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import {Button, Card} from 'react-bootstrap';
+import {Button, Card, Container} from 'react-bootstrap';
+import img4 from "../../assets/ssdLogo.jpg";
 
 
 const Products = () => {
     return (
+      <Container className="d">
+        <h1>Products</h1>
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js" />
+        <Card.Img variant="top" src={img4} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Headphones</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
@@ -15,6 +18,7 @@ const Products = () => {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
+      </Container>
   );
 }
 
